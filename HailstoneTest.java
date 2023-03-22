6b32ad2915ad7371da71ed5acce0f64b8f3cb26f
Nicholas-Phillipss <97136757+Nicholas-Phillipss@users.noreply.github.com>
2023-03-22 10:56:35 -0400

    Add files via upload

--- HailstoneTest.java
+++ HailstoneTest.java
@@ -1,25 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package stacksandqueues;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author Frank NKurunziza
+/*
+ * @author Frank NKurunziza, Nicholas Phillips, and Tyler Powell
+ * 
+ * The purpose of this code is to prompt the user to input a number and generate a Hailstone sequence using that number. 
+ * It creates an ArrayList to store the sequence and a Hailstone object to generate and manipulate the sequence. 
+ * The code then outputs the Hailstone sequence and the length of the longest sequence generated.
  */
+
+//This class provides a main method to test the Hailstone sequence generator.
 public class HailstoneTest {
     public static void main(String[] args) {
-        ArrayList<Integer> hailList=new ArrayList<>();
+        ArrayList<Integer> hailList=new ArrayList<>(); // Create a new ArrayList to store Hailstone sequence
         Scanner input=new Scanner(System.in);
-        System.out.println("Input a number from 1 to 10000");
-        System.out.println("--------------------------------");
+        System.out.println("Enter the largest number: ");
         int hailNumber=input.nextInt();
-        Hailstone hail=new Hailstone(hailNumber,hailList);
-        System.out.println("HailStone List: "+hail.hailNumber+" Longest sequence: "+hail.hailNumber.size());
+        Hailstone hail=new Hailstone(hailNumber,hailList); // Create a new Hailstone object with user input and hailList
+        System.out.println("HailStone List: "+hail.hailNumber+" Longest sequence: "+hail.hailNumber.size() + " Starting with: " + hail.hailNumber.get(0));
     }
 }
