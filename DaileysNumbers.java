8bb49f8ee25cff8768b2ab0354fb7653e2e8cd13
Tyler-Powell10 <56084361+Tyler-Powell10@users.noreply.github.com>
2023-04-21 17:30:27 -0400

    add

--- DaileysNumbers.java
+++ DaileysNumbers.java
@@ -1,5 +1,15 @@
+/*
+ * Tyler Powell
+ * Frank N
+ * Nick Philips 
+ * 
+ * add documentation here
+ * 
+ * 
+ */
+
+
 import java.util.*;
-import java.util.stream.Collectors;
 
 class TreeNode {
     int value;
@@ -12,120 +22,120 @@ class TreeNode {
 }
 
 public class DaileysNumbers {
-    public static void buildTree(TreeNode node, int depth, int parent, int grandparent) {
+    // recursivley builts the tree based on depth, parent, and grandparent values
+    public static void treeBuilder(TreeNode node, int depth, int parent, int grandparent) {
         if (depth == 0) {
             return;
         }
         node.left = new TreeNode(node.value + grandparent);
         node.right = new TreeNode(node.value + parent);
-        buildTree(node.left, depth - 1, node.value, parent);
-        buildTree(node.right, depth - 1, parent, node.value);
+        treeBuilder(node.left, depth - 1, node.value, parent);
+        treeBuilder(node.right, depth - 1, parent, node.value);
     }
 
-    public static List<Integer> preOrder(TreeNode node) {
+    // returns list of nodes in preorder
+    public static List<Integer> preOrderTraversal(TreeNode node) {
         List<Integer> result = new ArrayList<>();
         if (node != null) {
             result.add(node.value);
-            result.addAll(preOrder(node.left));
-            result.addAll(preOrder(node.right));
+            result.addAll(preOrderTraversal(node.left));
+            result.addAll(preOrderTraversal(node.right));
         }
         return result;
     }
 
-    public static List<Integer> inOrder(TreeNode node) {
+    // returns list of nodes in inorder
+    public static List<Integer> inOrderTraversal(TreeNode node) {
         List<Integer> result = new ArrayList<>();
         if (node != null) {
-            result.addAll(inOrder(node.left));
+            result.addAll(inOrderTraversal(node.left));
             result.add(node.value);
-            result.addAll(inOrder(node.right));
+            result.addAll(inOrderTraversal(node.right));
         }
         return result;
     }
 
-    public static List<Integer> postOrder(TreeNode node) {
+    // returns list of nodes in postorder
+    public static List<Integer> postOrderTraversal(TreeNode node) {
         List<Integer> result = new ArrayList<>();
         if (node != null) {
-            result.addAll(postOrder(node.left));
-            result.addAll(postOrder(node.right));
+            result.addAll(postOrderTraversal(node.left));
+            result.addAll(postOrderTraversal(node.right));
             result.add(node.value);
         }
         return result;
     }
 
-    public static void countOccurrences(TreeNode node, int[] counts) {
-        if (node == null) {
+    // keeps track of times a number is in the tree
+    public static void countTracker(TreeNode node, int[] counts, int depth, int maxDepth) {
+        if (node == null || depth > maxDepth) {
             return;
         }
-    
+
         if (node.value < counts.length) {
             counts[node.value]++;
         }
-    
-        countOccurrences(node.left, counts);
-        countOccurrences(node.right, counts);
-    }
-    
 
-    public static boolean[] missingNumbers(TreeNode node, int maxNumber) {
-        boolean[] present = new boolean[maxNumber + 1];
-        missingNumbersHelper(node, present);
-        return present;
+        countTracker(node.left, counts, depth + 1, maxDepth);
+        countTracker(node.right, counts, depth + 1, maxDepth);
     }
-    
-    public static void missingNumbersHelper(TreeNode node, boolean[] present) {
-        if (node == null) {
+
+    // prints out missing numbers in the tree
+    public static void missingNumbers(TreeNode node, boolean[] present, int depth, int maxDepth) {
+        if (node == null || depth > maxDepth) {
             return;
         }
-    
+
         if (node.value < present.length) {
             present[node.value] = true;
         }
-    
-        missingNumbersHelper(node.left, present);
-        missingNumbersHelper(node.right, present);
+
+        missingNumbers(node.left, present, depth + 1, maxDepth);
+        missingNumbers(node.right, present, depth + 1, maxDepth);
     }
     
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the tree depth (between 2 and 26): ");
+        System.out.print("Enter the tree depth between 2 and 26: ");
         int treeDepth = scanner.nextInt();
 
         TreeNode root = new TreeNode(3);
-        buildTree(root, treeDepth - 1, 2, 1);
+        treeBuilder(root, treeDepth - 1, 2, 1);
 
-        System.out.println("Traversal options:");
-        System.out.println("1. Preorder");
-        System.out.println("2. Inorder");
-        System.out.println("3. Postorder");
-        System.out.println("4. Missing numbers and counts");
-        System.out.println("5. Exit");
+        System.out.println("traversal options");
+        System.out.println("1. preOrderTraversal");
+        System.out.println("2. inOrderTraversal");
+        System.out.println("3. postOrderTraversal");
+        System.out.println("4. missing numbers and counts");
+        System.out.println("5. exit");
 
         int choice;
         do {
-            System.out.print("Enter your choice: ");
+            System.out.print("choose an option ");
             choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Preorder traversal: " + preOrder(root));
+                    System.out.println("preOrderTraversal traversal: " + preOrderTraversal(root));
                     break;
                 case 2:
-                    System.out.println("Inorder traversal: " + inOrder(root));
+                    System.out.println("inOrderTraversal traversal: " + inOrderTraversal(root));
                     break;
                 case 3:
-                    System.out.println("Postorder traversal: " + postOrder(root));
+                    System.out.println("postOrderTraversal traversal: " + postOrderTraversal(root));
                     break;
                 case 4:
                     int maxNumber = root.right.value;
-                    int[] counts = new int[maxNumber + 1];
-                    countOccurrences(root, counts);
+                    int
+                    [] counts = new int[maxNumber + 1];
+                    countTracker(root, counts, 1, treeDepth);
 
                     System.out.println("Counts:");
                     for (int i = 1; i <= maxNumber; i++) {
                         System.out.println(i + ": " + counts[i]);
                     }
-                    boolean[] present = missingNumbers(root, maxNumber);
+                    boolean[] present = new boolean[maxNumber + 1];
+                    missingNumbers(root, present, 1, treeDepth);
                     System.out.print("Missing: ");
                     for (int i = 1; i <= maxNumber; i++) {
                         if (!present[i]) {
@@ -135,15 +145,13 @@ public class DaileysNumbers {
                     System.out.println();
                     break;
                 case 5:
-                    System.out.println("Exiting...");
+                    System.out.println("exit");
                     break;
                 default:
-                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println("invalid input");
             }
         } while (choice != 5);
 
         scanner.close();
     }
 }
-
-                   
